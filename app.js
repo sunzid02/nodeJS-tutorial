325947536c68bbc533af44c1be900671e2b9f041
sunzid02 <sunzid02@gmail.com>
2019-05-25 11:25:54 +0600

    global object functionalities

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
-console.log('hey sunzid');
+console.log('welcome to node');
+
+// setTimeout(() => {
+//     console.log('3 secs have passed');  
+// }, 3000);
+
+
+// var time = 0;
+
+// var timer = setInterval(() => {
+    
+//     time += 2;
+//     console.log(time + ' have passed');    
+
+//     if (time > 10) 
+//     {
+//         clearInterval(timer);
+//     }
+
+// }, 2000);
+
+console.log(__dirname);
+console.log(__filename);
