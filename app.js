96cd24c2dc05ad335c2b6f7f3b5e21f2fa8bc793
sunzid02 <sunzid02@gmail.com>
2019-05-26 15:34:54 +0600

    file reading stream and chunk

--- app.js
+++ app.js
@@ -1,14 +1,28 @@
 var http = require('http');
+var fs = require('fs');
 
-var server = http.createServer((req, res) =>{
-    console.log('Request was mande' + req.url);
+var myReadStream = fs.createReadStream(__dirname + '/readMe.txt', 'utf8');
+
+
+// //lisening events
+myReadStream.on('data', function(chunk) {
+    console.log('.........................new chunk received.....................................................................');
+    console.log(chunk);
+    console.log(chunk.length);
+    console.log('\n \n \n');
+
     
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hey Ninjas');
 });
 
+// var server = http.createServer((req, res) =>{
+//     console.log('Request was mande' + req.url);
+    
+//     res.writeHead(200, { 'Content-Type': 'text/plain' });
+//     res.end('Hey Ninjas');
+// });
 
-// var server = http.createServer(onRequest).listen(8080);
-var port = 8083;
-server.listen(port, '127.0.0.1');
-console.log('Server started to port ' + port + '....');
+
+// // var server = http.createServer(onRequest).listen(8080);
+// var port = 8083;
+// server.listen(port, '127.0.0.1');
+// console.log('Server started to port ' + port + '....');
